a17ae06ac3617d4e7cda8bf732205be389fd7222
Chase Perritt <bricperr@ut.utm.edu>
2020-01-14 16:09:15 -0600

    The program has been converted to a Random Number Generator and Guessing Game

--- Project Test/Program.cs
+++ Project Test/Program.cs
@@ -8,10 +8,44 @@ namespace Project_Test
 {
     class Program
     {
-
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am thinking of a number between 0 and 100");
+            Program p = new Program();
+            int randNum = p.RandomNumber(0, 100);
+
+            int tries = 1;
+            int userGuess = 101;
+            string guess;
+
+            while(userGuess != randNum)
+            {
+                Console.WriteLine("Please enter a number between 0 and 100");
+                guess = Console.ReadLine();
+                userGuess = Convert.ToInt32(guess);
+
+                if (userGuess == randNum)
+                {
+                    Console.Write("Congratulations! You took ");
+                    Console.Write(tries);
+                    Console.WriteLine(" tries to find the number!");
+                    break;
+                }
+                else if (userGuess < randNum && userGuess >= 0)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else if (userGuess > randNum && userGuess <= 100)
+                {
+                    Console.WriteLine("Too High");
+                }
+                tries++;
+            }
             Console.ReadKey();
         }
     }
